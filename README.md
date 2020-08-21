cc0d1a0011c4ed7600b592680a2c448586c5cf66
Ken Ramos <vonnduven@gmail.com>
2020-08-20 18:29:47 -0700

    Update readme with stackblitz example link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Angular Animation via Directive
  Quick demonstration of using an Angular directive to animate elements in an easily extensible way.
 
+## Live demo
+Check it out [here](https://stackblitz.com/edit/angular-anim-directive-demo?)
--- src/app/animation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimationDirective } from './animation.directive';
-
-describe('AnimationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnimationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
